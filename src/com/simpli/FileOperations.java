8cd99c9d83b4d82cd2e56a33f0d7f42e8454082c
ManasaTadigotla <tmanasa4455@gmail.com>
2022-12-07 01:16:32 +0530

    src/com/simpli/FileOperations.java

--- src/com/simpli/FileOperations.java
+++ src/com/simpli/FileOperations.java
@@ -1,18 +1,6 @@
 package com.simpli;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 import java.io.BufferedReader;
 public class FileOperations
